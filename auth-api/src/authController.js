f990914fa1fe970430179c91b8822380d69f348c
Mehmet BOZKIRLIOĞLU <mehmet.bozkirlioglu@bitss.com.tr>
2024-10-04 11:56:25 +0300

    t1

--- auth-api/src/authController.js
+++ auth-api/src/authController.js
@@ -18,7 +18,7 @@ router.post('/register', async (req, res) => {
     await authService.register(password, Name, email);
     res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
-    res.status(500).json({ message: 'Error registering user' });
+    res.status(500).json({ message: 'Error registering user!!' });
   }
 });
 
